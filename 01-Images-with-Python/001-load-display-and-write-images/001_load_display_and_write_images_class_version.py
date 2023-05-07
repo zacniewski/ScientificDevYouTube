7a9b82ef90005bef1398a42e1163e667671a4dc4
Artur Zacniewski <a.zacniewski@gmail.com>
2023-05-07 20:46:56 +0200

    Refactored comments

--- 01-Images-with-Python/001-load-display-and-write-images/001_load_display_and_write_images_class_version.py
+++ 01-Images-with-Python/001-load-display-and-write-images/001_load_display_and_write_images_class_version.py
@@ -18,7 +18,18 @@ class ImageInformation:
     def get_image() -> cv2:
         """
         Load the image with argument parser
+
+        Parameters
+        ----------
+        None
+
+
+        Returns
+        -------
+        cv2
+            an image object from given path
         """
+
         ap = argparse.ArgumentParser()
         ap.add_argument("-i", "--image", required=True, help="path to the image to load")
         args = vars(ap.parse_args())
@@ -30,7 +41,18 @@ class ImageInformation:
         """
         Method to display the image width, height, and the number of channels
         We are assuming the image is a Numpy array
+
+        Parameters
+        ----------
+        image : cv2
+            The image object (Numpy's array)
+
+
+        Returns
+        -------
+        None
         """
+
         print(f"Width of the image: {image.shape[1]} pixels")
         print(f"Height of the image: {image.shape[0]} pixels")
         print(f"Number of channels: {image.shape[2]}")
@@ -39,7 +61,20 @@ class ImageInformation:
     def show_image(image, duration=500) -> None:
         """
         Method to show the image and wait given time or for a keypress
+
+        Parameters
+        ----------
+        image : cv2
+            The image object (Numpy's array)
+        duration : int
+            Time in miliseconds
+
+
+        Returns
+        -------
+        None
         """
+
         cv2.imshow("Image", image)
         cv2.waitKey(duration)
 
@@ -47,7 +82,20 @@ class ImageInformation:
     def save_image(image, new_name) -> None:
         """
         Method to save the image under the given name
+
+        Parameters
+        ----------
+        image : cv2
+            The image object (Numpy's array)
+        new_name : str
+            New name of the written image
+
+
+        Returns
+        -------
+        None
         """
+
         cv2.imwrite(f"{new_name}.jpg", image)
 
 
--- 01-Images-with-Python/001-load-display-and-write-images/001_load_display_and_write_images_function_version.py
+++ 01-Images-with-Python/001-load-display-and-write-images/001_load_display_and_write_images_function_version.py
@@ -5,7 +5,18 @@ import cv2
 def get_image() -> cv2:
     """
     Load the image with argument parser
+
+    Parameters
+    ----------
+    None
+
+
+    Returns
+    -------
+    cv2
+        an image object from given path
     """
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="path to the image to load")
     args = vars(ap.parse_args())
@@ -17,7 +28,18 @@ def display_info(image) -> None:
     """
     Method to display the image width, height, and the number of channels
     We are assuming the image is a Numpy array
+
+    Parameters
+    ----------
+    image : cv2
+        The image object (Numpy's array)
+
+
+    Returns
+    -------
+    None
     """
+
     print(f"Width of the image: {image.shape[1]} pixels")
     print(f"Height of the image: {image.shape[0]} pixels")
     print(f"Number of channels: {image.shape[2]}")
@@ -26,7 +48,20 @@ def display_info(image) -> None:
 def show_image(image, duration=500) -> None:
     """
     Method to show the image and wait given time or for a keypress
+
+    Parameters
+    ----------
+    image : cv2
+        The image object (Numpy's array)
+    duration : int
+        Time in miliseconds
+
+
+    Returns
+    -------
+    None
     """
+
     cv2.imshow("Image", image)
     cv2.waitKey(duration)
 
@@ -34,7 +69,20 @@ def show_image(image, duration=500) -> None:
 def save_image(image, new_name) -> None:
     """
     Method to save the image under the given name
+
+    Parameters
+    ----------
+    image : cv2
+        The image object (Numpy's array)
+    new_name : str
+        New name of the written image
+
+
+    Returns
+    -------
+    None
     """
+
     cv2.imwrite(f"{new_name}.jpg", image)
 
 
